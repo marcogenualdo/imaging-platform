7f340ae807ea9565c2d33fbe8e4cb02a640e65d6
mgenualdo <marco.genualdo@alten.it>
2021-03-26 19:44:57 +0100

    Netlify CMS.

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,19 +25,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-transformer-remark`,
-      options: { plugins: [`gatsby-plugin-netlify-cms-paths`] },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `content`,
-        path: `${__dirname}/content`,
-      },
-    },
-    // `gatsby-plugin-netlify-cms`,
-    // `gatsby-plugin-netlify-cms-paths`,
-    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
@@ -49,6 +36,19 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: { plugins: [`gatsby-plugin-netlify-cms-paths`] },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/content`,
+      },
+    },
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-netlify-cms-paths`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
