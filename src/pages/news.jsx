f7e37abdf1d78b93e210169fce85b606eb2bfc5a
mgenualdo <marco.genualdo@alten.it>
2021-05-07 21:10:35 +0200

    Production optimizations.

--- src/pages/news.jsx
+++ src/pages/news.jsx
@@ -8,7 +8,7 @@ import "../styles/style.scss";
 
 const NewsPage = ({ data }) => {
   return (
-    <Layout pageName="news & events">
+    <Layout pageName="news">
       <Section title="News at the platform">
         {data.news.nodes.map((item, index) => (
           <NewsEntry data={item} key={index} id={index} />
